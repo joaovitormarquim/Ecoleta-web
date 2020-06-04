b575fbbd1398c7a1b49e278c2d7ee86e69190899
João Vítor Marquim <jvmsmfn@cin.ufpe.br>
2020-06-03 23:22:55 -0300

    Handle form inputs

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -33,15 +33,22 @@ const CreatePoint = () => {
 
   const [initalPosition, setInitalPosition] = useState<[number, number]>([0, 0]);
 
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    whatsapp: ''
+  });
+
   const [selectedUf, setSelectedUf] = useState<string>('0');
   const [selectedCity, setSelectedCity] = useState<string>('0');
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
       const { latitude, longitude } = position.coords;
 
-      setInitalPosition([latitude, longitude])
+      setInitalPosition([latitude, longitude]);
     })
   }, []);
 
@@ -59,7 +66,7 @@ const CreatePoint = () => {
 
         setUfs(ufInitials);
       });
-  }, [])
+  }, []);
 
   useEffect(() => {
     // carregar as cidades sempre que a uf mudar
@@ -70,7 +77,7 @@ const CreatePoint = () => {
         setCities(cityNames);
       });
 
-  }, [selectedUf])
+  }, [selectedUf]);
 
   function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
     const uf = event.target.value;
@@ -87,6 +94,23 @@ const CreatePoint = () => {
       event.latlng.lat,
       event.latlng.lng
     ])
+  }
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+    setFormData({ ...formData,  [name]: value });
+  }
+
+  function handleSelectItem(id: number) {
+    const alreadySelected = selectedItems.findIndex(item => item === id);
+
+    if (alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter(item => item !== id );
+
+      setSelectedItems(filteredItems);
+    } else {
+      setSelectedItems([...selectedItems, id]);
+    }
   }
 
   return(
@@ -114,6 +138,7 @@ const CreatePoint = () => {
               type="text"
               name="name"
               id="name"
+              onChange={handleInputChange}
             />
           </div>
 
@@ -124,6 +149,7 @@ const CreatePoint = () => {
                 type="email"
                 name="email"
                 id="email"
+                onChange={handleInputChange}
               />
             </div>
 
@@ -133,6 +159,7 @@ const CreatePoint = () => {
                 type="text"
                 name="whatsapp"
                 id="whatsapp"
+                onChange={handleInputChange}
               />
             </div>
           </div>
@@ -195,7 +222,11 @@ const CreatePoint = () => {
           <ul className="items-grid">
             {items.map(item => {
               return(
-                <li key={item.id}>
+                <li
+                  key={item.id}
+                  onClick={() => handleSelectItem(item.id)}
+                  className={selectedItems.includes(item.id) ? 'selected' : ''}
+                >
                   <img src={item.image_url} alt={item.title}/>
                   <span>{item.title}</span>
                 </li>
